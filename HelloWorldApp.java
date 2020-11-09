ec0ff289a648a38b9390ac1d12dfb077af1c8d3c
mika <73530023+2everlove@users.noreply.github.com>
2020-11-09 11:43:40 +0900

    Add files via upload

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,5 +1,7 @@
-public class HelloWorldApp {
-	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+public class HelloWorldApp { //public 메소드 접근제어자, 누구든 접근가능
+	public static void main(String[] args) { //static:인스턴스 생성없이 실행가능
+		//void:리턴값없음 , string:문자열 나타내는 자바의 자료형
+		//args[]: String 자료형에 대한 변수명으로 args 뒤에[] 배열의미
+		System.out.println("Hello World!!");//println에 들어오는 문자열 값 화면 출력
 	}
 }
---
+++ javatutorials/numberstring/Number.java
@@ -0,0 +1,17 @@
+package javatutorials.numberstring;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println(1+2);
+		System.out.println(1.2+1.3);
+		System.out.println(5*2);
+		System.out.println(6/2);
+		System.out.println("say Hello \"Hello\"");
+		System.out.println("\nsay Hello\n \n\"Hello\"\n");
+		System.out.println("say Hello" + "\"Hello\"");
+		System.out.println('에');  // 문자는 ''
+		System.out.println("에버"); //문자열은 ""
+	}
+
+}
