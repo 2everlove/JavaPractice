8cfcdb046c6634dfdab7cc523fc44b0e147a82db
mika <73530023+2everlove@users.noreply.github.com>
2020-11-15 23:11:00 +0900

    Add files via upload

---
+++ Data_and_operation/Datatype.java
@@ -0,0 +1,7 @@
+public class Datatype {
+	public static void main(String[] args) {
+		System.out.println(6); //number
+		System.out.println("6"); //string
+		System.out.println("1111".length()); //text length=4
+	}
+}
