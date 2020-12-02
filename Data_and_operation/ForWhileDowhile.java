b3984e7c2b13b4eacf383af09d6a7bb1bfa102c7
mika <73530023+2everlove@users.noreply.github.com>
2020-12-02 19:24:13 +0900

    Add files via upload
    Example) for, while, do-while

---
+++ Data_and_operation/ForWhileDowhile.java
@@ -0,0 +1,27 @@
+
+public class ForWhileDowhile {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		
+		do {
+			i++;
+			System.out.println(i);
+		} while(i<5);
+		//--
+		System.out.println("*");
+		for(int b=1; b<=5; b++) {
+			System.out.println(b);
+		}
+		//
+		System.out.println("-");
+		int a=1;
+		
+		while(a<=5) {
+			System.out.println(a);
+			a++;
+		}
+	}
+
+}
